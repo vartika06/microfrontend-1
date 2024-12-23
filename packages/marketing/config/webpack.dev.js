02dd95d1b1b3c3e4280dec6a42188c37195d3622
Vartika Patel <vartika@borneo.io>
2024-12-23 22:12:18 +0530

    chore: add container deployment script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -20,9 +20,6 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap"
       },
       shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
     })
   ]
 };
