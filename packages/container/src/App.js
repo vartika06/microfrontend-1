53af4eb19127503c22c095dedd29104611acbb06
Vartika Patel <vartika@borneo.io>
2024-12-23 14:22:31 +0530

    feat: add container and marketing mfe setup

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingComponent from "./components/MarketingComponent";
+
+export default () => {
+  return (
+    <div>
+      <h1>Container</h1>
+      <hr />
+      <MarketingComponent />
+    </div>
+  );
+};
